33fd734a6196d65358c4ec70df9d53e152243371
Mark Hoad <markdhoad@gmail.com>
2016-10-28 12:43:16 +0100

    Added admin flag to User model and created basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create an admin user
+unless User.exists?(email: 'admin@ticketee.com')
+  User.create!(email: 'admin@ticketee.com',
+               password: 'password',
+               password_confirmation: 'password',
+               admin: true)
+end
+
+# Create a regular user
+unless User.exists?(email: 'viewer@ticketee.com')
+  User.create!(email: 'viewer@ticketee.com',
+               password: 'password',
+               password_confirmation: 'password')
+end
+
+['Sublime Text 3', 'Internet Explorer'].each do |name|
+  unless Project.exists?(name: name)
+    Project.create!(name: name,
+                    description: "A sample project about #{name}")
+  end
+end
